5d2dcbb98718905409b1f42058568873418f97e4
Aayush <aayush9152@gmail.com>
2020-07-04 17:00:09 +0530

    coub component

--- client/src/Components/Coub/Coub.js
+++ client/src/Components/Coub/Coub.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import axios from 'axios';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faRetweet } from '@fortawesome/free-solid-svg-icons';
+
 import './Coub.css';
 
 const Coub = ({ url, id }) => {
@@ -9,11 +12,13 @@ const Coub = ({ url, id }) => {
     const [ author, setAuthor ] = useState({});
     const [ coubDetails, setCoubDetails ] = useState({});
     const [ coubId, setCoubId ] = useState(id);
+    const [ userDetails, setUserDetails ] = useState({});
 
     useEffect(() => {
         const getCoubDetails = async () => {
             const coubRes = await axios.get(`/api/getCoubDetails?coubid=${coubId}`);
-            setCoubDetails(coubRes.data);
+            setCoubDetails(coubRes.data.coubDetails);
+            setUserDetails(coubRes.data.userDetails);
         }
 
         getCoubDetails();
@@ -28,8 +33,43 @@ const Coub = ({ url, id }) => {
                 controls = { true }
             />
 
-            <div className = "coub-details">
-                
+            <div className = "coub-details-hero">
+                <div className = "creator-details">
+                    <img src = { userDetails.dp }/>
+                    <h2>{ coubDetails.caption }</h2>
+                    <div className = "creator">
+                        <p>Created By</p>
+                        <h3>{ userDetails.name }</h3>
+                    </div>
+                </div>
+
+                <div className = "stats-hero">
+                    <div className = "stat">
+                        <FontAwesomeIcon icon = { faHeart }/>
+                        <p>{ coubDetails.hearts }</p>
+                    </div>
+
+                    <div className = "stat">
+                        <FontAwesomeIcon icon = { faRetweet }/>
+                        <p>0</p>
+                    </div>
+
+                    <div className = "stat">
+                        <FontAwesomeIcon icon = { faRetweet }/>
+                        <p>0</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className = "tags-views-hero">
+                <div className = "tags">
+                    { coubDetails.tags }
+                </div>
+
+                <div className = "views">
+                    <p>{ coubDetails.views} Views</p>
+                </div>
             </div>
         </div>
     )
